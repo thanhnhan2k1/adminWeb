814c98654558e3c29720fedae27a0942eee0a17a
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2023-12-16 04:01:57 +0700

    Update WoodController.java

--- src/main/java/com/example/controller/WoodController.java
+++ src/main/java/com/example/controller/WoodController.java
@@ -40,7 +40,7 @@ import com.example.model.Wood;
 @RequestMapping("/wood")
 public class WoodController {
 	private RestTemplate rest=new RestTemplate();
-	private String url="http://localhost:8082";
+	private String url="https://server2-production-29ab.up.railway.app";
 	//private String url="https://da-server2-production.up.railway.app";
 	@GetMapping
 	private String get(Model model, HttpSession session , @RequestParam(name="category", defaultValue = "0", required = false)int category) {
