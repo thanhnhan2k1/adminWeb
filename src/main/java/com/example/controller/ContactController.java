f3a388e14ebcb446347976d2d0b5d44e60f515d4
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2023-12-16 04:00:14 +0700

    Update ContactController.java

--- src/main/java/com/example/controller/ContactController.java
+++ src/main/java/com/example/controller/ContactController.java
@@ -18,7 +18,7 @@ import com.example.model.Contact;
 @RequestMapping("/contact")
 public class ContactController {
 	private RestTemplate rest=new RestTemplate();
-	private String url="http://localhost:8082";
+	private String url="https://server2-production-29ab.up.railway.app";
 	//private String url="https://da-server2-production.up.railway.app";
 	
 	@GetMapping
