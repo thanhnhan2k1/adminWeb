7c127ae8f0c1184163771777e326056583fd44c6
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2023-12-16 04:01:18 +0700

    Update StatController.java

--- src/main/java/com/example/controller/StatController.java
+++ src/main/java/com/example/controller/StatController.java
@@ -22,7 +22,7 @@ import com.example.model.UsedService;
 @RequestMapping("/stat")
 public class StatController {
 	private RestTemplate rest=new RestTemplate();
-	private String url="http://localhost:8082";
+	private String url="https://server2-production-29ab.up.railway.app";
 	//private String url="https://da-server2-production.up.railway.app";
 	@GetMapping("/getServiceStatShow")
 	private String show(HttpSession session) {
