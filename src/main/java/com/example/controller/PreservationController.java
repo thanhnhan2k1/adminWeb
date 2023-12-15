47dc4b74d81f1cfeffc7c60e9a613e63d5d4b129
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2023-12-16 04:01:08 +0700

    Update PreservationController.java

--- src/main/java/com/example/controller/PreservationController.java
+++ src/main/java/com/example/controller/PreservationController.java
@@ -22,7 +22,7 @@ import com.example.model.Preservation;
 public class PreservationController {
 
 	private RestTemplate rest=new RestTemplate();
-	private String url="http://localhost:8082";
+	private String url="https://server2-production-29ab.up.railway.app";
 	//private String url="https://da-server2-production.up.railway.app";
 	@GetMapping
 	private String get(@RequestParam(name="key", defaultValue = "", required = false)String key, HttpSession session, Model model) {
